f9f5811356b463d81cae4778ace04374c36830fb
Reza Kohzadvand <151601046+Kohzadvand@users.noreply.github.com>
2024-06-21 16:56:53 +0330

    Create Example_6.java

---
+++ Example_6.java
@@ -0,0 +1,41 @@
+public class Kohzadvand {
+
+   public static void main(String[] args)
+
+   {
+       System.out.println("Example 6 ");
+      System.out.println("THIS PROGRAM HAS BEEN WRITTEN BE REZA KOHZADVAND ");
+      System.out.println("****************************");
+      System.out.println();
+   
+
+       int x, y;
+
+       Integer num1, num2;
+
+       num1 = 20;
+
+       num2 = 11;
+
+       System.out.println("num1 = " + num1 + ", num2 = " + num2);
+
+       x = 2 * num1 + num2;
+
+       y = num1 + num2;
+
+       System.out.println("x = " + x + ", y = " + y);
+
+       String s = new String("12");
+
+       x = Integer.parseInt(s);
+
+       s = s + num1.toString() +  num2.toString();       
+
+       System.out.println("Convert int to String and concate :" + s);
+
+
+       
+
+   }
+
+}
