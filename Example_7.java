c845834b56e080d7fc3c04bb67b4467484ca3c9a
Reza Kohzadvand <151601046+Kohzadvand@users.noreply.github.com>
2024-06-21 17:00:36 +0330

    Create Example_7.java

---
+++ Example_7.java
@@ -0,0 +1,27 @@
+import static java.lang.Character.*;
+
+public class Kohzadvand {
+
+    public static void main(String[] args)
+
+    {
+     System.out.println("Example 7 ");
+      System.out.println("THIS PROGRAM HAS BEEN WRITTEN BE REZA KOHZADVAND ");
+      System.out.println("****************************");
+      System.out.println();
+    
+
+    	System.out.println("Uppercase('r') = " + toUpperCase('r'));
+
+    	System.out.println("isLetter('k') = " + isLetter('k'));
+
+    	System.out.println("isLetter('+') = " + isLetter('+'));
+
+    	System.out.println("isUpperCase('t') = " + isUpperCase('t'));
+
+    	System.out.println("isSpaceChar(' ') = " + isSpaceChar(' '));
+    
+
+    }
+
+}
